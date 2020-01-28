07ddd74f5afdb8f3a21195090d7083adff462023
Tedd Hansen <tedd@konge.net>
2020-01-28 15:07:24 +0100

    Visualizing app

---
+++ src/Tedd.FastNoise.Visualizer/UI/Dimensions/D1.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tedd.FastNoise.Visualizer.UI.Dimensions
+{
+    /// <summary>
+    /// Interaction logic for D1.xaml
+    /// </summary>
+    public partial class D1 : UserControl
+    {
+        public D1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Tedd.FastNoise.Visualizer/UI/Dimensions/D2.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tedd.FastNoise.Visualizer.UI.Dimensions
+{
+    /// <summary>
+    /// Interaction logic for D2.xaml
+    /// </summary>
+    public partial class D2 : UserControl
+    {
+        public D2()
+        {
+            InitializeComponent();
+        }
+    }
+}
